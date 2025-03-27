0bd95276ea657320e955fd3bbabcc86cd76185ef
oiko@DESKTOP <908900359@qq.com>
2025-03-27 11:48:25 +0800

    3.27 morning

--- server.cpp
+++ server.cpp
@@ -8,20 +8,65 @@
 #include <fmt/format.h>
 #include <string.h>
 
+struct socket_address_fatptr{
+  struct sockaddr *m_addr;
+  socklen_t m_addrlen;
+};
+
+struct address_resolved_entry{
+  struct addrinfo *m_curr = nullptr;
+
+  socket_address_fatptr get_address()const{
+    return {m_curr->ai_addr,m_curr->ai_addrlen};
+  }
+
+  int create_socket() const{
+    int sockfd = socket(m_curr->ai_family,m_curr->ai_socktype,m_curr->ai_protocol);
+    if(sockfd == -1){
+      fmt::println("socket:{}",strerror(errno));
+      throw;
+    }
+    return sockfd;
+  }
+
+
+};
+
+struct address_resolver
+{
+  /* data */
+  struct addrinfo *m_head = nullptr;
+
+  void resolve(std::string const &name,std::string const &service){
+    int err = getaddrinfo(name.c_str(),service.c_str(),NULL,&m_head);
+    if(err != 0){
+      fmt::println("getaddrinfo:{} {}",gai_strerror(err),err);
+      throw;
+    }
+  }
+  address_resolved_entry get_first_entry(){
+    return {m_head};
+  }
+
+  address_resolver() = default;
+
+  address_resolver(address_resolver &&that):m_head(that.m_head){
+    that.m_head = nullptr;
+  }
+
+  ~address_resolver(){
+    if(m_head){
+      freeaddrinfo(m_head);
+    }
+  }
+};
+
+
 int main(){
     setlocale(LC_ALL,"zh_CN.UTF-8");
-    struct addrinfo *addrinfo;
-    int res = getaddrinfo("localhost","80",NULL,&addrinfo);
-    if(res != 0){
-        perror("getaddrinfo");
-        return -1;
-    }
-    
-    int sockfd = socket(-1,addrinfo->ai_socktype,addrinfo->ai_protocol);
-    if(sockfd == -1){
-        fmt::println("errno: {}",strerror(errno));
-        return -1;
-    }
+    address_resolver resolver;
+    auto entry = resolver.resolve("127.0.0.0","80");
+    int sockfd = entry
 
 
 }
